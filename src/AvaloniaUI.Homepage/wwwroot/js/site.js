041c5e18b57832d1f45a603c3a7466bbbc59db26
Steven Kirk <grokys@gmail.com>
2020-03-31 14:37:48 +0200

    Manually apply hljs.
    For some reason `initHighlightingOnLoad` only works on chrome.

--- src/AvaloniaUI.Homepage/wwwroot/js/site.js
+++ src/AvaloniaUI.Homepage/wwwroot/js/site.js
@@ -1,7 +1,10 @@
 $(function () {
     'use strict';
 
-    hljs.initHighlightingOnLoad();
+    $('pre code').each(function (i, block) {
+        hljs.highlightBlock(block);
+    }); 
+
     anchors.add();
 
     var navbar = $('#navbar');
