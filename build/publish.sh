47475a0d1ee06a9c30bf41cbc21adc135282116b
Steven Kirk <grokys@gmail.com>
2020-04-23 15:24:26 +0200

    Fixes.

--- build/publish.sh
+++ build/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 dotnet publish --configuration Release
-rsync -avz --delete src/AvaloniaUI.Homepage//bin/Release/netcoreapp3.1/publish/ grokys@95.216.209.17:/var/www/avaloniaui.net
+rsync -avz --delete src/AvaloniaUI.Net/bin/Release/netcoreapp3.1/publish/ grokys@95.216.209.17:/var/www/avaloniaui.net
